2ac760f3eadd61c87ad05551daa29f3ab7f91f98
nidhal-98 <nidhalsalim98@gmail.com>
2023-05-15 09:41:01 +0400

    Packages are created

---
+++ src/main/java/com/example/Healthcare/management/system/HealthcareManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.Healthcare.management.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthcareManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthcareManagementSystemApplication.class, args);
+	}
+
+}
